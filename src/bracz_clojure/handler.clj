bd520aa2e16e4f3d9c82615639e67e6cb2424d0d
tvedeane <1138746+tvedeane@users.noreply.github.com>
2021-11-14 12:23:53 +0100

    Define main method
    Define production profile
    
    Replace profile
    
    Use Heroku tutorial https://github.com/heroku/clojure-getting-started
    
    Fix namespace
    
    Define main function location
    
    Remove :main
    
    Revert dev setup

--- src/bracz_clojure/handler.clj
+++ src/bracz_clojure/handler.clj
@@ -1,13 +1,22 @@
 (ns bracz-clojure.handler
   (:require [compojure.core :refer :all]
+            [compojure.handler :refer [site]]
             [compojure.route :as route]
             [ring.util.response :as resp]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [clojure.java.io :as io]
+            [ring.adapter.jetty :as jetty]
+            [environ.core :refer [env]]))
 
 (defroutes app-routes
   (GET "/" [] (resp/redirect "/index.html"))
+  (GET "/hello" [] "Hello!")
   (route/resources "/")
   (route/not-found "Not Found"))
 
 (def app
   (wrap-defaults app-routes site-defaults))
+
+(defn -main [& [port]]
+  (let [port (Integer. (or port (env :port) 5000))]
+    (jetty/run-jetty (site #'app) {:port port :join? false})))
