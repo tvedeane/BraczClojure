92887e240de6d98b2b5c5b7920627f77bf70fb78
tvedeane <1138746+tvedeane@users.noreply.github.com>
2021-11-14 12:23:53 +0100

    Define main method
    Define production profile
    
    Replace profile
    
    Use Heroku tutorial https://github.com/heroku/clojure-getting-started
    
    Fix namespace
    
    Define main function location
    
    Remove :main
    
    Revert dev setup

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/bracz-clojure-standalone.jar clojure.main -m bracz-clojure.handler
--- project.clj
+++ project.clj
@@ -5,11 +5,14 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  ;[org.clojure/tools.nrepl "0.2.13"]
                  [compojure "1.6.1"]
-                 [ring/ring-defaults "0.3.2"]]
-  :plugins [[lein-ring "0.12.5"]]
-  :ring {:handler bracz-clojure.handler/app
-         ;:nrepl {:start? true}
-         }
-  :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.2"]]}})
+                 [ring/ring-jetty-adapter "1.7.1"]
+                 [ring/ring-defaults "0.3.2"]
+                 [environ "1.1.0"]]
+  :plugins [[environ/environ.lein "0.3.1"]
+            [lein-ring "0.12.5"]]
+  :ring {:handler bracz-clojure.handler/app}
+  :hooks [environ.leiningen.hooks]
+  :uberjar-name "bracz-clojure-standalone.jar"
+  :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                                  [ring/ring-mock "0.3.2"]]}
+             :production {:env {:production true}}})
